efc469c6935e28e4bc432e39141111dca25d4e15
Yuval Adam <_@yuv.al>
2018-06-16 11:32:11 +0300

    Initial proof of concept

---
+++ smil.lua
@@ -0,0 +1,21 @@
+local utils = require 'mp.utils'
+local msg = require 'mp.msg'
+local options = require 'mp.options'
+
+mp.add_hook("on_load" or "on_load_fail", 50, function ()
+    local url = mp.get_property("stream-open-filename", "")
+    msg.warn("Attempting to fetch SMIL")
+    msg.error(url)
+
+    res = utils.subprocess({
+	args = {"curl", "-s", url}
+    })
+    body = res["stdout"]
+    smilurl = string.match(body, '<SmilURL name="default">(.*)</SmilURL>')
+
+    if not (smilurl == nil) then
+	msg.warn("Found smilurl, playing")
+	msg.warn(smilurl)
+        mp.set_property("stream-open-filename", smilurl)
+    end
+end)
