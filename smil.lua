08c56d5ab32ec29ed70edd0e871d7324f3878618
Yuval Adam <_@yuv.al>
2018-06-16 12:45:28 +0300

    Skip non-HTTP streams

--- smil.lua
+++ smil.lua
@@ -4,18 +4,21 @@ local options = require 'mp.options'
 
 mp.add_hook("on_load" or "on_load_fail", 50, function ()
     local url = mp.get_property("stream-open-filename", "")
-    msg.warn("Attempting to fetch SMIL")
-    msg.error(url)
 
-    res = utils.subprocess({
-	args = {"curl", "-s", url}
-    })
-    body = res["stdout"]
-    smilurl = string.match(body, '<SmilURL name="default">(.*)</SmilURL>')
+    if not (string.match(url, '^http') == nil) then
+        msg.warn("Attempting to fetch SMIL")
+        msg.error(url)
 
-    if not (smilurl == nil) then
-	msg.warn("Found smilurl, playing")
-	msg.warn(smilurl)
-        mp.set_property("stream-open-filename", smilurl)
+        res = utils.subprocess({
+            args = {"curl", "-s", url}
+        })
+        body = res["stdout"]
+        smilurl = string.match(body, '<SmilURL name="default">(.*)</SmilURL>')
+
+        if not (smilurl == nil) then
+	    msg.warn("Found smilurl, playing")
+	    msg.warn(smilurl)
+            mp.set_property("stream-open-filename", smilurl)
+        end
     end
 end)
